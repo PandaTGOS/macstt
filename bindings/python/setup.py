70292947f93fbbe37ebc0445b6c48dc75cb985f3
Sakhi S Panda <sakhisaswatpanda@gmail.com>
2026-07-19 03:01:47 +0530

    Build platform-specific macOS wheels

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,3 +1,25 @@
 from setuptools import setup
 
-setup()
+try:
+    from setuptools.command.bdist_wheel import bdist_wheel
+except ImportError:
+    from wheel.bdist_wheel import bdist_wheel
+
+
+class PlatformWheel(bdist_wheel):
+    """Build a platform-specific, Python-version-independent wheel."""
+
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+
+    def get_tag(self):
+        _, _, plat = super().get_tag()
+        return "py3", "none", plat
+
+
+setup(
+    cmdclass={
+        "bdist_wheel": PlatformWheel,
+    }
+)
